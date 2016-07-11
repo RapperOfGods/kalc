afc9d08c8f3ab35448c963dae8840329b60b9e37
RapperOfGods <andrei.zik@mail.ru>
2016-07-11 11:15:49 +0600

    Basic operations

--- kalcForGodsofIVT/kalc/kalc/Form1.Designer.cs
+++ kalcForGodsofIVT/kalc/kalc/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace kalc
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,85 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.multiplication = new System.Windows.Forms.Button();
+            this.division = new System.Windows.Forms.Button();
+            this.subraction = new System.Windows.Forms.Button();
+            this.addition = new System.Windows.Forms.Button();
+            this.arg1 = new System.Windows.Forms.TextBox();
+            this.arg2 = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // multiplication
             // 
-            this.button1.Location = new System.Drawing.Point(72, 136);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "*";
-            this.button1.UseVisualStyleBackColor = true;
+            this.multiplication.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.multiplication.Location = new System.Drawing.Point(72, 136);
+            this.multiplication.Name = "multiplication";
+            this.multiplication.Size = new System.Drawing.Size(75, 23);
+            this.multiplication.TabIndex = 0;
+            this.multiplication.Text = "*";
+            this.multiplication.UseVisualStyleBackColor = true;
+            this.multiplication.Click += new System.EventHandler(this.multiplication_Click);
             // 
-            // button2
+            // division
             // 
-            this.button2.Location = new System.Drawing.Point(169, 136);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "/";
-            this.button2.UseVisualStyleBackColor = true;
+            this.division.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.division.Location = new System.Drawing.Point(169, 136);
+            this.division.Name = "division";
+            this.division.Size = new System.Drawing.Size(75, 23);
+            this.division.TabIndex = 1;
+            this.division.Text = "/";
+            this.division.UseVisualStyleBackColor = true;
+            this.division.Click += new System.EventHandler(this.division_Click);
             // 
-            // button3
+            // subraction
             // 
-            this.button3.Location = new System.Drawing.Point(72, 178);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "-";
-            this.button3.UseVisualStyleBackColor = true;
+            this.subraction.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.subraction.Location = new System.Drawing.Point(72, 178);
+            this.subraction.Name = "subraction";
+            this.subraction.Size = new System.Drawing.Size(75, 23);
+            this.subraction.TabIndex = 2;
+            this.subraction.Text = "-";
+            this.subraction.UseVisualStyleBackColor = true;
+            this.subraction.Click += new System.EventHandler(this.subraction_Click);
             // 
-            // button4
+            // addition
             // 
-            this.button4.Location = new System.Drawing.Point(169, 178);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "+";
-            this.button4.UseVisualStyleBackColor = true;
+            this.addition.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addition.Location = new System.Drawing.Point(169, 178);
+            this.addition.Name = "addition";
+            this.addition.Size = new System.Drawing.Size(75, 23);
+            this.addition.TabIndex = 3;
+            this.addition.Text = "+";
+            this.addition.UseVisualStyleBackColor = true;
+            this.addition.Click += new System.EventHandler(this.addition_Click);
             // 
-            // textBox1
+            // arg1
             // 
-            this.textBox1.Location = new System.Drawing.Point(28, 31);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(274, 20);
-            this.textBox1.TabIndex = 4;
+            this.arg1.Location = new System.Drawing.Point(28, 31);
+            this.arg1.Name = "arg1";
+            this.arg1.Size = new System.Drawing.Size(274, 20);
+            this.arg1.TabIndex = 4;
             // 
-            // textBox2
+            // arg2
             // 
-            this.textBox2.Location = new System.Drawing.Point(28, 57);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(274, 20);
-            this.textBox2.TabIndex = 5;
+            this.arg2.Location = new System.Drawing.Point(28, 57);
+            this.arg2.Name = "arg2";
+            this.arg2.Size = new System.Drawing.Size(274, 20);
+            this.arg2.TabIndex = 5;
             // 
-            // textBox3
+            // result
             // 
-            this.textBox3.Location = new System.Drawing.Point(28, 99);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(274, 20);
-            this.textBox3.TabIndex = 6;
+            this.result.Location = new System.Drawing.Point(28, 99);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(274, 20);
+            this.result.TabIndex = 6;
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(318, 213);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(326, 213);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.arg2);
+            this.Controls.Add(this.arg1);
+            this.Controls.Add(this.addition);
+            this.Controls.Add(this.subraction);
+            this.Controls.Add(this.division);
+            this.Controls.Add(this.multiplication);
+            this.Name = "MainForm";
             this.Text = "kalc";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -115,13 +123,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button multiplication;
+        private System.Windows.Forms.Button division;
+        private System.Windows.Forms.Button subraction;
+        private System.Windows.Forms.Button addition;
+        private System.Windows.Forms.TextBox arg1;
+        private System.Windows.Forms.TextBox arg2;
+        private System.Windows.Forms.TextBox result;
     }
 }
 
--- kalcForGodsofIVT/kalc/kalc/Form1.cs
+++ kalcForGodsofIVT/kalc/kalc/Form1.cs
@@ -10,11 +10,63 @@ using System.Windows.Forms;
 
 namespace kalc
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
+
+
+
+
+
+
+
+        private void multiplication_Click(object sender, EventArgs e)
+        {
+
+            double argument1 = Convert.ToDouble(arg1.Text);
+            double argument2 = Convert.ToDouble(arg2.Text);
+
+            double temp_result = argument1*argument2;
+
+            result.Text = temp_result.ToString();
+
+        }
+
+        private void division_Click(object sender, EventArgs e)
+        {
+            double argument1 = Convert.ToDouble(arg1.Text);
+            double argument2 = Convert.ToDouble(arg2.Text);
+
+            double temp_result = argument1 / argument2;
+
+            result.Text = temp_result.ToString();
+        }
+
+        private void subraction_Click(object sender, EventArgs e)
+        {
+            double argument1 = Convert.ToDouble(arg1.Text);
+            double argument2 = Convert.ToDouble(arg2.Text);
+
+            double temp_result = argument1 - argument2;
+
+            result.Text = temp_result.ToString();
+        }
+
+        private void addition_Click(object sender, EventArgs e)
+        {
+            double argument1 = Convert.ToDouble(arg1.Text);
+            double argument2 = Convert.ToDouble(arg2.Text);
+
+            double temp_result = argument1 + argument2;
+
+            result.Text = temp_result.ToString();
+        }
+
+
+
+
     }
 }
